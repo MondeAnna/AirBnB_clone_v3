b59816092885c5b01f0593242a266a4ea3d5a497
MondeAnna <monde.anna@gmail.com>
2024-05-28 04:39:36 +0200

    api (app): add `404` error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,9 +3,15 @@
 
 
 import os
+
+
+from flask import make_response
+from flask import jsonify
 from flask import Flask
-from models import storage
+
+
 from api.v1.views import app_views
+from models import storage
 
 
 HOST = os.getenv("HBNB_API_HOST", "0.0.0.0")
@@ -21,6 +27,14 @@ def teardown_app(exception):
     """teardown app session"""
 
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found():
+    """404 page not found error"""
+
+    response = jsonify({"error": "Not found"})
+    return make_response(response, 404
 
 
 if __name__ == "__main__":
