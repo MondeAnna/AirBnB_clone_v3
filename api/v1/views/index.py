c1a2fcfca79ad7c2072b89909aac885ab33a4b44
MondeAnna <monde.anna@gmail.com>
2024-05-28 04:46:25 +0200

    api (views): correction -> jsonfiy ... I RTFD

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,13 +9,14 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route("/status", methods["GET"], strict_slashes=False)
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status_route():
     """JSON Serialiasble Object with site status"""
 
-    return jsonify({"status": "OK"})
+    return jsonify(status= "OK")
 
-@app_views.route("/stats", methods["GET"], strict_slashes=False)
+
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
 def stats_route():
     """JSON Serialiasble Object with site stats"""
 
